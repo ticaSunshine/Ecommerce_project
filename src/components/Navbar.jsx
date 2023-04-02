16d23b1f1b4947e9fa73a1554790af16ac437cdc
Tica Sunshine <vranic.tica@gmail.com>
2023-04-02 20:20:44 +0200

    setup react router, unfinished

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import {ShoppingCart} from 'phosphor-react'
+
+export const Navbar = () => {
+  return (
+    <div className='navbar'>
+         <div className='links'>
+            <Link to="/">Shop</Link>
+            <Link to='/cart'><ShoppingCart size={32}/></Link>
+         </div>
+         
+         </div>
+  )
+}
+//https://www.youtube.com/watch?v=tEMrD9t85v4 13:57

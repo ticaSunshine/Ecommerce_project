0d24f2c6ad1e41540c8d89a64dd86c6e6c60b79b
Tica Sunshine <vranic.tica@gmail.com>
2023-04-16 18:09:14 +0200

    finish add to cart logic in product page

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {ShopContext} from '../../context/shop-context'
 
 export default function Product(props) {
 const {id, productName, price, productImage} =props.data;
+const {addToCart, cartItems} = useContext(ShopContext);
+
+const cartItemAmount = cartItems[id]
   return (
     <div className='product'>
         <img src={productImage} alt={id}/>
@@ -9,6 +13,7 @@ const {id, productName, price, productImage} =props.data;
             <p><b>{productName}</b></p>
             <p>{price}€</p>
         </div>
+        <button className='addToCartBttn' onClick={()=>addToCart(id)}>Add {cartItemAmount > 0 && <>({cartItemAmount})</> }</button>
     </div>
   )
 }
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {PRODUCTS} from '../../products';
 import Product from './product';
+import './shop.css';
 
 export const Shop = () => {
   return (
@@ -8,7 +9,7 @@ export const Shop = () => {
 
     <div className='shop'>
         <div className='shopTitle'>
-            <h1>Online shop</h1>
+            <h1>NFT shop</h1>
         </div>
         <div className='products'>
             {PRODUCTS.map((product) => {
